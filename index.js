c4f18e1a2fd6fe62da5f0e1e9d9d91b8a7e23062
Milena Kari <milenakari@yahoo.com>
2019-10-23 01:39:51 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,3 +3,7 @@ findMatching = (drivers, str) => {
   let results = drivers.filter(driver => driver.toLowerCase() === str.toLowerCase())
   return results
 }
+
+fuzzyMatch = (drivers, str) => {
+  return drivers.filter
+}
